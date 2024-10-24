fe8c629c266960aac1951a7f33d04871d2df5e9d
ImJasc <173310950+ImJasc@users.noreply.github.com>
2024-10-23 20:28:01 -0700

    created the array that will hold the books, created the constructor that will create new books

---
+++ script.js
@@ -0,0 +1,28 @@
+const library = [
+    {
+        "title": "Mistborn",
+        "author": "Brandon Sanderson",
+        "genre": "High Fantasy",
+        "read": "yes"
+    },
+    {
+        "title": "The Way of Kings",
+        "author": "Brandon Sanderson",
+        "genre": "High Fantasy",
+        "read": "yes"
+    }
+]
+
+function book(title, author, genre, read){
+    this.title = title,
+    this.author = author,
+    this.genre = genre,
+    this.read = read
+}
+
+function addBookToLibrary(){
+
+}
+
+//user interface
+
