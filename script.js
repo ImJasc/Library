5391e7bc34028a91d4a21a028f0b1609b94f409b
ImJasc <173310950+ImJasc@users.noreply.github.com>
2024-10-26 17:21:08 -0700

    added styles to the modal and evvent listeners to show and hide the modal

--- script.js
+++ script.js
@@ -21,8 +21,26 @@ function book(title, author, genre, read){
 }
 
 function addBookToLibrary(){
-
+    
 }
 
 //user interface
 
+
+const dialog = document.querySelector("dialog");
+const addButton = document.querySelector("button#button");
+
+addButton.addEventListener("click", showDialog =>{
+    dialog.showModal();
+})
+
+dialog.addEventListener("click", ({ target: dialog }) => {
+    if (dialog.nodeName === "DIALOG") {
+        dialog.classList.add('hide');
+        dialog.addEventListener('webkitAnimationEnd', function(){
+            dialog.classList.remove('hide');
+            dialog.close();
+            dialog.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+    }, false);
+    }
+});
