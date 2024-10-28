a8499a142616f02bc31d39aedca3c7e29d87ba23
ImJasc <173310950+ImJasc@users.noreply.github.com>
2024-10-27 22:11:39 -0700

    removed the placeholders books in library created the function to add new books to library

--- script.js
+++ script.js
@@ -1,46 +1,60 @@
-const library = [
-    {
-        "title": "Mistborn",
-        "author": "Brandon Sanderson",
-        "genre": "High Fantasy",
-        "read": "yes"
-    },
-    {
-        "title": "The Way of Kings",
-        "author": "Brandon Sanderson",
-        "genre": "High Fantasy",
-        "read": "yes"
-    }
-]
+const library = []
 
-function book(title, author, genre, read){
-    this.title = title,
-    this.author = author,
-    this.genre = genre,
-    this.read = read
+class book {
+    constructor(title, author, genre, read) {
+        this.title = title,
+        this.author = author,
+        this.genre = genre,
+        this.read = read;
+    }
 }
 
 function addBookToLibrary(){
     
 }
 
-//user interface
-
-
 const dialog = document.querySelector("dialog");
 const addButton = document.querySelector("button#button");
+const submitButton = document.querySelector("button#submit");
+const addBookForm = document.querySelector("form#newBook");
 
 addButton.addEventListener("click", showDialog =>{
+    addBookForm.reset();
     dialog.showModal();
 })
 
-dialog.addEventListener("click", ({ target: dialog }) => {
-    if (dialog.nodeName === "DIALOG") {
+dialog.addEventListener("click", closeModal);
+
+submitButton.addEventListener("click", submit);
+
+function submit (e){
+    if(addBookForm.checkValidity() === true){
+        e.preventDefault();
+        const title = document.querySelector("input#title").value;
+        const author = document.querySelector("input#author").value;
+        const genre = document.querySelector("input#genre").value;
+        const isRead = document.querySelector("input#read").checked;
+        library.push(new book(title, author, genre, isRead));
+        console.log(library);
         dialog.classList.add('hide');
         dialog.addEventListener('webkitAnimationEnd', function(){
             dialog.classList.remove('hide');
             dialog.close();
             dialog.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
-    }, false);
+        }, false);
+    }else{
+        addBookForm.reportValidity();
     }
-});
+}
+
+function closeModal({ target: dialog }){
+    if (dialog.nodeName === "DIALOG") {        
+        dialog.classList.add('hide');
+        dialog.addEventListener('webkitAnimationEnd', function(){
+            dialog.classList.remove('hide');
+            dialog.close();
+            dialog.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+        }, false);
+    }
+}
+
